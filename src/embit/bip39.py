87a704b0fb5ba9de2b6aeb41be82bbcd765c6588
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2024-05-11 07:50:08 -0400

    best speedup is a WORDINDEX dict

--- src/embit/bip39.py
+++ src/embit/bip39.py
@@ -6,6 +6,8 @@ from .wordlists.bip39 import WORDLIST
 
 PBKDF2_ROUNDS = const(2048)
 
+WORDINDEX = {word: i for i, word in enumerate(WORDLIST)}
+
 
 def mnemonic_to_bytes(mnemonic: str, ignore_checksum: bool = False, wordlist=WORDLIST):
     words = mnemonic.strip().split()
@@ -15,21 +17,19 @@ def mnemonic_to_bytes(mnemonic: str, ignore_checksum: bool = False, wordlist=WOR
     accumulator = 0
     for word in words:
         try:
-            accumulator = (accumulator << 11) + wordlist.index(word)
+            if wordlist is WORDLIST:
+                accumulator = (accumulator << 11) + WORDINDEX[word]
+            else:
+                accumulator = (accumulator << 11) + wordlist.index(word)
         except Exception:
             raise ValueError("Word '%s' is not in the dictionary" % word)
 
     entropy_length_bits = len(words) * 11 // 33 * 32
     checksum_length_bits = len(words) * 11 // 33
-    total_length_bytes = (entropy_length_bits + checksum_length_bits) // 8
-    if checksum_length_bits % 8:
-        total_length_bytes += 1
-        accumulator = accumulator << 8 - checksum_length_bits
-    raw = accumulator.to_bytes(total_length_bytes, "big")
-    data = raw[ : entropy_length_bits // 8]
-    checksum = int.from_bytes(raw[entropy_length_bits // 8 : ], "big") >> 8 - checksum_length_bits
-    hashed_entropy = hashlib.sha256(data).digest()
-    computed_checksum = int.from_bytes(hashed_entropy, "big") >> 256 - checksum_length_bits
+    checksum = accumulator & (2**checksum_length_bits -1)
+    accumulator >>= checksum_length_bits
+    data = accumulator.to_bytes(entropy_length_bits // 8, "big")
+    computed_checksum = hashlib.sha256(data).digest()[0] >> 8 - checksum_length_bits
 
     if not ignore_checksum and checksum != computed_checksum:
         raise ValueError("Checksum verification failed")
